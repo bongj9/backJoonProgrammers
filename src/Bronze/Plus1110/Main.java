2b789a039bd98c22e788896b17395658801e288f
bongj9 <lollypark501@naver.com>
2023-11-02 22:07:33 +0900

    feat:백준1110

---
+++ src/Bronze/Plus1110/Main.java
@@ -0,0 +1,17 @@
+package Bronze.Plus1110;
+
+public class Main {
+    public static void main(String[] args) {
+        int n = 26;
+        int res = n;
+        int tmp = 0;
+        int cnt = 0; //횟수 초기화 0
+        do {
+            tmp = res / 10 + res % 10;
+            res = res % 10 + 10 + tmp % 10;
+            cnt++;
+        }
+        while (n != res);
+        System.out.println(cnt);
+    }
+}
