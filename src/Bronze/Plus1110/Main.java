96247f9f4ac815b3d6ee105ebeef8ca154977816
bongj9 <lollypark501@naver.com>
2023-11-03 13:47:23 +0900

    refac:roop문수정

--- src/Bronze/Plus1110/Main.java
+++ src/Bronze/Plus1110/Main.java
@@ -1,17 +1,19 @@
 package Bronze.Plus1110;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        int n = 26;
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
         int res = n;
-        int tmp = 0;
-        int cnt = 0; //횟수 초기화 0
+        int tmp;
+        int cnt = 0; // 횟수 초기화 0
         do {
-            tmp = res / 10 + res % 10;
-            res = res % 10 + 10 + tmp % 10;
+            tmp = res / 10 + res % 10; // 각 자릿수를 더함
+            res = (res % 10) * 10 + tmp % 10; // 새로운 수를 생성
             cnt++;
-        }
-        while (n != res);
+        } while (n != res);
         System.out.println(cnt);
     }
 }
