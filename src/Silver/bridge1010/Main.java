a63d0f8646e6bbf36c730741cfb6e382c29f6c68
bongj9 <lollypark501@naver.com>
2023-11-14 15:23:40 +0900

    feat:StringTokenizer사용법추가

--- src/Silver/bridge1010/Main.java
+++ src/Silver/bridge1010/Main.java
@@ -8,7 +8,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(bf.readLine());
-        for (int i = 0; i < T; i++) {
+        for (int i = 0; i < T; i++) { //변수 ,final->대문자,변수명 확실하게
+            //split->배열로 쓸떄,
+            //StringYtokenizer st =new String
             String[] numbers = bf.readLine().split(" ");
             int N = Integer.parseInt(numbers[0]);
             int M = Integer.parseInt(numbers[1]);
@@ -25,6 +27,14 @@ public class Main {
         return result;
     }
 }
+/*    StringTokenizer st = new StringTokenizer(bf.readLine()); 띄어쓰기로 변환
+    int a = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < T; i++) {
+        StringTokenizer numbers = new StringTokenizer(bf.readLine());
+        int N = Integer.parseInt(numbers.nextToken());
+        int M = Integer.parseInt(numbers.nextToken());
+        System.out.println(combination(N, M));
+        }*/
 /* 스택오버 플로우가 나서 조합값이 잘 안나옴 팩토리얼값이 너무 커져서 감당이 안되서 그런듯
     public static long combination(int n, int m) {
         return factorial(m) / (factorial(n)*factorial(m - n));
