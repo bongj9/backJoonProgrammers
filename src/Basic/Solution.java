1559226ab5326449b7651358cd082460b5618079
bongj9 <lollypark501@naver.com>
2023-11-19 23:09:10 +0900

    SSaF

---
+++ src/Basic/Solution.java
@@ -0,0 +1,82 @@
+package Basic;
+
+///////////////////////////////////////////////////////////////////////////////////////////// Test
+//기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+//아래 표준 입출력 예제 필요시 참고하세요.
+//표준 입력 예제
+//int a = sc.nextInt();                 // int 변수 1개 입력받는 예제
+//double b = sc.nextDouble();           // double 변수 1개 입력받는 예제
+//long AB = sc.nextLong();              // long 변수 1개 입력받는 예제
+//String str = sc.next();               // 문자열 1개 입력받는 예제
+//char c = str.charAt(0);               // 입력받은 문자열에서 char 변수 1개 가져오는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+//표준 출력 예제
+//int a = 0;
+//double b = 1.0;
+//long AB = 12345678901234567L;
+//String str = "ABCDEFG";
+//char c = 'b';
+//System.out.println(a);                // int 변수 1개 출력하는 예제
+//System.out.println(b);                // double 변수 1개 출력하는 예제
+//System.out.println(AB);               // long 변수 1개 출력하는 예제
+//System.out.println(str);              // 문자열 1개 출력하는 예제
+//System.out.println(c);                // char 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+*/
+class Solution
+{   //전역변수 선언
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); //테스트 할 갯수
+        StringTokenizer st = new StringTokenizer(" ");
+        //반복
+        for (int test_case = 1; test_case <= T; test_case++)
+        {
+            //N의 값  = 배열의 갯수인데
+            int N = sc.nextInt();
+            int[] arr = new int[N];
+            //입력받아 배열을 만들었다
+            for (int i = 0; i < N ; i++) {
+                arr[i] = sc.nextInt();
+            }
+            //구현한 함수를 넣는다
+
+            //입력받은걸 나누는것
+
+
+
+            /////////////////////////////////////////////////////////////////////////////////////////////
+			/*
+			   이 부분에 여러분의 알고리즘 구현이 들어갑니다.
+			*/
+            /////////////////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+            // 표준출력(화면)으로 답안을 출력합니다.
+            System.out.print("#" + test_case);
+            System.out.print();
+            //나의 알고리즘 출력
+        }
+
+        sc.close(); // 사용이 끝난 스캐너 객체를 닫습니다.
+    }
+
+    public static int calculate () { //int n
+
+    }
+}
