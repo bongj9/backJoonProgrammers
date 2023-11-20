ac8e03c0aa82b86a4af97fdee0003595715f5cb0
bongj9 <lollypark501@naver.com>
2023-11-20 16:33:19 +0900

    feat:알고리즘구현

--- src/Silver/walk1459/Main.java
+++ src/Silver/walk1459/Main.java
@@ -4,22 +4,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String line = br.readLine();
-        String[] numbersString = line.split(" ");
-        long [] numbers = new long[numbersString.length];
-        //숫자를 나눠놓는것
-        for (int i = 0; i < numbersString.length; i++) {
-            numbers[i] = Integer.parseInt(numbersString[i]);
-        }
+        String[] line = br.readLine().split(" ");
+
+        long x = Long.parseLong(line[0]);
+        long y = Long.parseLong(line[1]);
+        long w = Long.parseLong(line[2]);
+        long s = Long.parseLong(line[3]);
 
 
 
     }
-    //알고리즘 구현
-    public static int walktime() {
 
+    //알고리즘 구현
+    public static void cal(Long x, Long y, Long w, Long s) {
+        long temp1 = (x + y) * w;
+        long temp2 = 0;
+        if ((x + y) % 2 == 0) {
+            temp2 = Math.max(x, y) * s;
+
+        }
+        else{
+            temp2 = (Math.max(x, y) * s )+ w;
+        }
+        long temp3 = (Math.min(x, y))*s+(Math.abs(x-y))*w;
+
+        System.out.println(Math.min(temp1, Math.min(temp2, temp3)));
     }
 }
+
