70a84b824be2202e707fcd5e368f5ae0e67efe96
bongj9 <lollypark501@naver.com>
2023-11-23 12:32:26 +0900

    feat:IO입출력생성

---
+++ src/Silver/Atm11399/Main.java
@@ -0,0 +1,15 @@
+package Silver.Atm11399;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+        int[] people = new int[testcase]; //생성
+
+    }
+
+}
