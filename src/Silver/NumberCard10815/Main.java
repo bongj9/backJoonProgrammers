4e927b3097755aecc8d0b0e37ac6598cfdaa1a37
bongj9 <lollypark501@naver.com>
2023-11-24 14:09:44 +0900

    feat:cardMatchmethod

--- src/Silver/NumberCard10815/Main.java
+++ src/Silver/NumberCard10815/Main.java
@@ -3,6 +3,7 @@ package Silver.NumberCard10815;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.concurrent.LinkedBlockingDeque;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,6 +13,16 @@ public class Main {
         int userTestCase = Integer.parseInt(br.readLine());
         String[] UserInput = br.readLine().split(" ");
 
+    }
+
+    public static void CardMatch(int[] arr, int[] arr2) {
+        for (int i = 0; i <arr.length; i++) {
+            for (int j = 0; j <arr2.length ; j++) {
+                if (arr[i] ==arr2 [j]){
+                    System.out.println(1);
+                } else System.out.println(0);
+            }
+        }
 
     }
 
