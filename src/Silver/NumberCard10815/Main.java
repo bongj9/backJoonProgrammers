ac7bf80c86699b88de08191d11d2c49aad571ad8
bongj9 <lollypark501@naver.com>
2023-11-24 14:04:50 +0900

    feat:10815입출력구현

---
+++ src/Silver/NumberCard10815/Main.java
@@ -0,0 +1,18 @@
+package Silver.NumberCard10815;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine()); //처음 N개를 넣어줌
+        String [] Sysinput = br.readLine().split(" ");
+        int userTestCase = Integer.parseInt(br.readLine());
+        String[] UserInput = br.readLine().split(" ");
+
+
+    }
+
+}
