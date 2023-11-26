a177bea8a6bd525cd957094fc424f0215ccf283f
bongj9 <lollypark501@naver.com>
2023-11-26 16:12:55 +0900

    feat:stack구조구현

--- src/Silver/Zero10773/Main.java
+++ src/Silver/Zero10773/Main.java
@@ -4,12 +4,26 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testcase = Integer.parseInt(br.readLine());
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < testcase; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0 && !stack.isEmpty()) {
+                stack.pop();
+            }
+            else {
+                stack.push(num);
+            }
+
+        }
+
+
+        }
+
     }
 
-}
