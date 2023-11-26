3a0425e5a1d41a6e701b95d9bbabf012fbb2fe32
bongj9 <lollypark501@naver.com>
2023-11-26 15:31:16 +0900

    feat:입출력

--- src/Silver/Zero10773/Main.java
+++ src/Silver/Zero10773/Main.java
@@ -1,8 +1,15 @@
 package Silver.Zero10773;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
     }
 
 }
