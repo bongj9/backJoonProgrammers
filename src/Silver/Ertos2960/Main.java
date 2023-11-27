86a977dad6ddf49bcc0af0e60f48ce3288a398b6
bongj9 <lollypark501@naver.com>
2023-11-27 12:25:01 +0900

    feat:입출력구현

--- src/Silver/Ertos2960/Main.java
+++ src/Silver/Ertos2960/Main.java
@@ -1,7 +1,13 @@
 package Silver.Ertos2960;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int K = Integer.parseInt(br.readLine());
     }
 }
