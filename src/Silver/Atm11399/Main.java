95076b15952a3cb1f6d9d2121130c7989c3f6756
bongj9 <lollypark501@naver.com>
2023-12-01 22:10:39 +0900

    feat:ATM

--- src/Silver/Atm11399/Main.java
+++ src/Silver/Atm11399/Main.java
@@ -32,8 +32,6 @@ public class Main {
         System.out.println(total);
 
     }
-
-
     public static void sort(int[] arr) {
         for (int i = 0; i < arr.length - 1; i++) {
             for (int j = 0; j < arr.length - i - 1; j++) {
