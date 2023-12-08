095ac6e3399fd72b7caa869fd6df22824bec6f3f
bongj9 <lollypark501@naver.com>
2023-12-08 11:20:01 +0900

    refact:로직

--- src/Silver/Paper2563/Main.java
+++ src/Silver/Paper2563/Main.java
@@ -9,17 +9,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testcase = Integer.parseInt(br.readLine());
-        int[] X = new int[testcase];
-        int[] Y = new int[testcase];
+        Boolean[][] grid = new Boolean[100][100];
+        int overlappedArea = 0;
         for (int i = 0; i < testcase; i++) {
             String[] input = br.readLine().split(" ");
-            X[i] = Integer.parseInt(input[0]);
-            Y[i] = Integer.parseInt(input[1]);
+            int X = Integer.parseInt(input[0]);
+            int Y = Integer.parseInt(input[1]);
+
+            // 겹치는 부분 확인하며 영역 표시
+            for (int x = X; x < X + 10; x++) {
+                for (int y = Y; y < Y + 10; y++) {
+                    if (grid[x][y]) {
+                        overlappedArea++; // 겹치는 부분 면적 증가
+                    } else {
+                        grid[x][y] = true; // 처음 차지하는 영역 표시
+                    }
+                }
+            }
         }
-        Arrays.sort(X);
-        Arrays.sort(Y);
+
+
         //총량 공식
-        int result = 100 * testcase -(); // -겹친부분
+        int result = 100 * testcase -overlappedArea; // -겹친부분
+        System.out.println(result);
 
     }
 }
