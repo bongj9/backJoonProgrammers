bcc133cf774a0d20a238e8e35bf5e927c0f6f9f3
bongj9 <lollypark501@naver.com>
2023-12-18 08:36:43 +0900

    feat:완전탐색으로구현

--- src/Bronze/divide2231/Main.java
+++ src/Bronze/divide2231/Main.java
@@ -6,7 +6,25 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int test = Integer.parseInt(br.readLine());
+    }
 
+    public static int findConstructor(int N) {
+        for (int i = 1; i < N; i++) {
+            int sum = i;
+            int current = i;
 
+            // 각 자리수를 더하는 부분
+            while (current > 0) {
+                sum += current % 10; // 현재 자리수 더하기
+                current /= 10; // 다음 자리수로 이동
+            }
+
+            // 분해합이 N과 같은 경우
+            if (sum == N) {
+                return i; // 생성자 반환
+            }
+        }
+        return 0; // 생성자가 없는 경우
     }
 }
+
