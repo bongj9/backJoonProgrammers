5859fbe255d34cc673968c535536e7e6962daf87
bongj9 <lollypark501@naver.com>
2024-01-01 15:48:50 +0900

    feat:입출력구현

---
+++ src/BackJ/Silver/Paper1802/Main.java
@@ -0,0 +1,18 @@
+package BackJ.Silver.Paper1802;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int size = 0;
+    static int thick = 0;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testcase; i++) {
+            int 
+        }
+
+    }
+}
