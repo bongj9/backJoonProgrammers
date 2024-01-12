53216251d95f3dd7140a291ca3f422886ba320f2
bongj9 <lollypark501@naver.com>
2024-01-12 23:51:44 +0900

    feat:지원자거리확인

--- src/programmers/Lv2/Distance/Solution.java
+++ src/programmers/Lv2/Distance/Solution.java
@@ -4,4 +4,17 @@ public class Solution {
     private static final int dx[] = {0, -1, 1, 0}; //아래 왼 오 위
     private static final int dy[] = {-1, 0, 0, 1};
 
-}
+    private boolean isNextToVolunteer(char[][] room, int x, int y, int exclude) {
+        for (int d = 0; d < 4; d++) {
+            if (d == exclude) continue;
+
+            int nx = x + dx[d];
+            int ny = y + dy[d];
+            if (ny < 0 || ny >= room.length || nx < 0 || nx >= room[ny].length) continue;
+            if (room[ny][nx] == 'P')
+                return true;
+            }
+            return false;
+        }
+
+    }
