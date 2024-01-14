4575d03921e7928dab9f810e675ebab0763de49d
bongj9 <lollypark501@naver.com>
2024-01-14 23:33:01 +0900

    feat:메인로직

--- src/programmers/Lv2/Distance/Solution.java
+++ src/programmers/Lv2/Distance/Solution.java
@@ -46,4 +46,21 @@ public class Solution {
         }
         return true;
     }
+
+    public int[] solution(String[][] places) {
+        int[] answer = new int[places.length];
+        for (int i = 0; i < answer.length; i++) {
+            String[] place = places[i];
+            char[][] room = new char[place.length][];
+            for (int j = 0; j < room.length; j++) {
+                room[j] = place[j].toCharArray();
+            }
+            if (isDistanced(room)) {
+                answer[i] = 1;
+            }else {
+                answer[i] = 0;
+            }
+        }
+        return answer;
+    }
 }
