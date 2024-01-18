ff4ef9a5479ab4a69de98db713176ad97030d803
bongj9 <lollypark501@naver.com>
2024-01-18 23:02:16 +0900

    feat:입출력구현

--- src/BackJ/Silver/captain2775/main.java
+++ src/BackJ/Silver/captain2775/main.java
@@ -1,7 +1,12 @@
 package BackJ.Silver.captain2775;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     }
 }
