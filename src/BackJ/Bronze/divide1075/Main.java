bb172d1976d513e7f35fbdddf34252d7532766db
bongj9 <lollypark501@naver.com>
2024-01-24 23:36:23 +0900

    백준1075

---
+++ src/BackJ/Bronze/divide1075/Main.java
@@ -0,0 +1,24 @@
+package BackJ.Bronze.divide1075;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int f = Integer.parseInt(br.readLine());
+
+        n -= n % 100;
+
+        while(true) {
+            if(n % f != 0) {
+                n++;
+            } else {
+                System.out.printf("%02d", n % 100);
+                break;
+            }
+        }
+    }
+}
