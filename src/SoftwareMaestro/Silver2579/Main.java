b87880e743821e040199fea0b909e6f5b9effd14
bongj9 <lollypark501@naver.com>
2024-02-20 21:04:36 +0900

    refact:2579객체지향

--- src/SoftwareMaestro/Silver2579/Main.java
+++ src/SoftwareMaestro/Silver2579/Main.java
@@ -29,4 +29,60 @@ public class Main {
     }
 }
 
+/*
+import java.io.BufferedReader;
+        import java.io.IOException;
+        import java.io.InputStreamReader;
+
+// 계단 오르기 로직을 담당하는 클래스
+class StairClimber {
+    private int[] step;
+    private int[] dp;
+    private int numberOfSteps;
+
+    public StairClimber(int numberOfSteps) {
+        this.numberOfSteps = numberOfSteps;
+        this.step = new int[numberOfSteps + 1];
+        this.dp = new int[numberOfSteps + 1];
+    }
+
+    public void setStepScores(int stepIndex, int score) {
+        this.step[stepIndex] = score;
+    }
+
+    public int calculateMaxScore() {
+        if (numberOfSteps < 1) {
+            return 0;
+        }
+
+        dp[1] = step[1];
+        if (numberOfSteps >= 2) {
+            dp[2] = step[1] + step[2];
+        }
+
+        for (int i = 3; i <= numberOfSteps; i++) {
+            dp[i] = Math.max(dp[i-2] + step[i], dp[i-3] + step[i-1] + step[i]);
+        }
+
+        return dp[numberOfSteps];
+    }
+}
+
+// 메인 클래스
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+
+        StairClimber climber = new StairClimber(t);
+
+        for (int i = 1; i <= t; i++) {
+            int score = Integer.parseInt(br.readLine());
+            climber.setStepScores(i, score);
+        }
+
+        System.out.println(climber.calculateMaxScore()); // 최대 점수 출력
+    }
+}
+*/
 
