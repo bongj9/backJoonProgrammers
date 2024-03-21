3d66e76034b04990cc0a25f513d52ef0b3c271e3
bongj9 <lollypark501@naver.com>
2024-03-21 14:08:36 +0900

    백준 11478

---
+++ src/BackJ/Silver/stringCount11478/Main.java
@@ -0,0 +1,24 @@
+package BackJ.Silver.stringCount11478;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String s = br.readLine();
+
+        Set<String> substrings = new HashSet<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            for (int j = i + 1; j <= s.length(); j++) {
+                substrings.add(s.substring(i, j));
+            }
+        }
+
+        System.out.println(substrings.size());
+    }
+}
