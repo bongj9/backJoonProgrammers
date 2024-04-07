7a85a8c3540dd58fcd53fdc315693c0bd59171e4
bongj9 <lollypark501@naver.com>
2024-04-07 23:58:06 +0900

    백준 4779

---
+++ src/BackJ/Silver/canto4779/Main.java
@@ -0,0 +1,16 @@
+package BackJ.Silver.canto4779;
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testCase = Integer.parseInt(br.readLine());
+
+
+    }
+
+    void recursive(int n) {
+
+    }
+}
