621350b990fdf2a3dbcbe775d771fe805def3bcb
bongj9 <lollypark501@naver.com>
2024-04-08 14:42:56 +0900

    백준 4779

--- src/BackJ/Silver/canto4779/Main.java
+++ src/BackJ/Silver/canto4779/Main.java
@@ -4,13 +4,30 @@ import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int testCase = Integer.parseInt(br.readLine());
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextInt()) {
+            int n = sc.nextInt();
+            char[] arr = new char[(int) Math.pow(3, n)];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = '-';
+            }
+            recursive(arr, 0, arr.length, ' ');
+            System.out.println(arr);
+        }
 
 
     }
-
-    void recursive(int n) {
+    //빈칸 만들어주기
+    private static void recursive(char[] arr, int start ,int end, char fill) {
+        if (end - start == 1) {
+            return;
+        }
+        int segment = (end - start) / 3;
+        for (int i = start+ segment; i < start + segment * 2; i++) {
+            arr[i] = fill;
+        }
+        recursive(arr, start, start + segment, fill);
+        recursive(arr, start + segment * 2, end, fill);
 
     }
 }
