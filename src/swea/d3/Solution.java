3300b3309a89fe4952ead9fa40a7b23cdb1c7116
bongj9 <lollypark501@naver.com>
2024-04-18 16:57:15 +0900

    swea d3

---
+++ src/swea/d3/Solution.java
@@ -0,0 +1,16 @@
+package swea.d3;
+
+public class Solution {
+    public static int solution(int X) {
+        int answer = 0;
+        int[][] month = new int[12][31];
+        int[] lastday = {31,28,30,31,30,31,31,30,31,30,31};
+        for (int[] i : month) {
+            for (int i1 : i) {
+
+            }
+
+        }
+        return answer;
+    }
+}
