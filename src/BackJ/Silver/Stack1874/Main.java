6527902268ed48c66fc0a71d79456841834fa9a2
bongj9 <lollypark501@naver.com>
2024-05-16 21:25:17 +0900

    백준 1874

---
+++ src/BackJ/Silver/Stack1874/Main.java
@@ -0,0 +1,36 @@
+package BackJ.Silver.Stack1874;
+
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+        StringBuilder stringBuilder = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
+        int j = 1;
+        boolean err = false;
+
+        for (int i = 0; i < testcase; i++) {
+            int n = Integer.parseInt(br.readLine());
+            while (j <= n) {
+                stack.push(j);
+                stringBuilder.append("+").append("\n");
+                j++;
+            }
+            if (stack.peek() == n) {
+                stack.pop();
+                stringBuilder.append("-").append("\n");
+            } else {
+                err = true;
+                break;
+            }
+        }
+        if (err) {
+            System.out.println("NO");
+        } else {
+            System.out.println(stringBuilder.toString());
+        }
+    }
+}
