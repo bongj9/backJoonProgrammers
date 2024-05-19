832bacd18f41276ce74e7c9f8a6d4cc8e945dc4f
bongj9 <lollypark501@naver.com>
2024-05-19 10:18:16 +0900

    백준1012

---
+++ src/BackJ/Silver/organic1012/Main2.java
@@ -0,0 +1,12 @@
+package BackJ.Silver.organic1012;
+
+import java.io.*;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int testcase = Integer.parseInt(br.readLine());
+
+
+    }
+}
