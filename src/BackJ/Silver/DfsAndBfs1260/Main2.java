e202e946dded5f16373e99e074fe870c48ba9df9
bongj9 <lollypark501@naver.com>
2024-05-25 23:49:17 +0900

    백준 1260 입출력만구현

---
+++ src/BackJ/Silver/DfsAndBfs1260/Main2.java
@@ -0,0 +1,17 @@
+package BackJ.Silver.DfsAndBfs1260;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()); //정점의 갯수
+        int M = Integer.parseInt(st.nextToken()); //간선의개수
+        int V = Integer.parseInt(st.nextToken()); //시작 번호
+
+    }
+}
