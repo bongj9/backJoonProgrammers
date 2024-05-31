5f41b92f3f88f0605fe582ce65124bd90f489aa9
Bong J <114319597+bongj9@users.noreply.github.com>
2024-05-31 23:34:14 +0900

    [Silver V] Title: 이름 궁합, Time: 268 ms, Memory: 129072 KB -BaekjoonHub

---
+++ 백준/Silver/15312. 이름 궁합/이름 궁합.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String A = br.readLine();
+        String B = br.readLine();
+
+        int[] a = {3, 2, 1, 2, 3, 3, 2, 3, 3, 2, 2, 1, 2, 2, 1, 2, 2, 2, 1, 2, 1, 1, 1, 2, 2, 1};
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < A.length(); i++) {
+            list.add(a[A.charAt(i) - 'A']);
+            list.add(a[B.charAt(i) - 'A']);
+        }
+        
+        while (list.size() > 2) {
+            ArrayList<Integer> newList = new ArrayList<>();
+            for (int i = 0; i < list.size() - 1; i++) {
+                newList.add((list.get(i) + list.get(i + 1)) % 10);
+            }
+            list = newList;
+        }
+        
+        System.out.println("" + list.get(0) + list.get(1));
+    }
+}
