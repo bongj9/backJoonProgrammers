3d1a7fcd31bee0b668834619165a2acae8cb1c69
Bong J <114319597+bongj9@users.noreply.github.com>
2024-06-03 09:34:11 +0900

    [Silver IV] Title: 제로, Time: 212 ms, Memory: 23256 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -8,21 +8,22 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testcase = Integer.parseInt(br.readLine());
         Stack<Integer> stack = new Stack<>();
-
         for (int i = 0; i < testcase; i++) {
             int num = Integer.parseInt(br.readLine());
             if (num == 0 && !stack.isEmpty()) {
                 stack.pop();
-            } else {
+            }
+            else {
                 stack.push(num);
             }
-        }
 
+        }
         int sum = 0;
         while (!stack.isEmpty()) {
             sum += stack.pop();
         }
 
         System.out.println(sum);
+        }
+
     }
-}
